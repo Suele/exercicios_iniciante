5ccf575fb4eed413ab9c2733eea85be48e9d88cb
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2017-11-14 02:05:14 -0200

    Menor número com quatro valores

---
+++ Exercicios Iniciante/src/iniciante/MenorNumeroComquatroValores.java
@@ -0,0 +1,15 @@
+//Faça um programa que receba 4 valores e retorne o menor entre eles.
+
+package iniciante;
+
+import java.util.Scanner;
+
+public class MenorNumeroComquatroValores {
+	public static void main(String[] args) {
+		
+		Scanner dadosDoTeclado = new Scanner(System.in);
+		
+		
+
+	}
+}
