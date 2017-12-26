07628e3fb25ae6d0f9395e324c4a3dcba58b2b68
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2017-12-26 14:25:09 -0200

    Foi adicionado atualização de config.

---
+++ Exercicios Iniciante/src/iniciante/Variaveis.java
@@ -0,0 +1,16 @@
+package iniciante;
+
+public class Variaveis {
+	public static void main(String[] args) {
+		
+		int idade;
+		double salario;
+		char primeiraLetraDoNome;
+		
+		idade = 28;
+		salario = 3.500;
+		primeiraLetraDoNome = 'S';
+		
+		System.out.println("Idade: " + idade + " Salario: " + salario + " Primeira Letra do Nome: " + primeiraLetraDoNome);
+	}
+}
