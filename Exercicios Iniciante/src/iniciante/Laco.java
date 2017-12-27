38af8d85f145ef93785ad9934fe1d66e8c22d8a5
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2017-12-27 00:30:32 -0200

    Laço de 150 ate 300

---
+++ Exercicios Iniciante/src/iniciante/Laco.java
@@ -0,0 +1,16 @@
+// Laço de 150 ate 300.
+
+package iniciante;
+
+public class Laco {
+	public static void main(String[] args) {
+		
+		int i = 150;
+		
+		while (i <= 300) {
+			System.out.println(i);
+			i++;
+		}
+		
+	}
+}
