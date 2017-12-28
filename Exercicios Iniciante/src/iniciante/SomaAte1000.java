3c07ad30fd614e157a7b70df202a5ed34fc1e3ef
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2017-12-27 23:57:21 -0200

    O arquivo SomaAte1000 soma todos os números de 1 áte 1000

---
+++ Exercicios Iniciante/src/iniciante/SomaAte1000.java
@@ -0,0 +1,15 @@
+package iniciante;
+
+public class SomaAte1000 {
+	public static void main(String[] args) {
+
+		int soma = 0;
+
+		for (int i = 1; i <= 1000; i++) {
+			soma = i + i;
+
+		}
+		System.out.println(soma);
+
+	}
+}
