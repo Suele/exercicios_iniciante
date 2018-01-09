a1a829d2d450a2ae2f364c6e237740897d9fb3dd
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-01-09 12:23:34 -0200

    Foi adicionado o arquivo Nota.java que verifica se um aluno foi aprovado ou reprovado.

---
+++ Exercicios Iniciante/src/iniciante/Nota.java
@@ -0,0 +1,18 @@
+// Algoritmo que verifica se o aluno foi aprovado ou reprovado.
+
+package iniciante;
+
+public class Nota {
+	public static void main(String[] args) {
+
+		int nota;
+		nota = 50;
+
+		if (nota >= 60) {
+			System.out.println("Aluno Aprovado.");
+		} else {
+			System.out.println("Aluno Reprovado.");
+		}
+	}
+
+}
