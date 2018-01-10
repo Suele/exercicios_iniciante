3c2d64e7b8dd55a962747988c6cafb34207b230d
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-01-10 00:26:38 -0200

    Foi comentado o código do arquivo Divisão.java e adicionado outro código com o bloco do for para dar mais legibilidade ao código.

--- Exercicios Iniciante/src/iniciante/Divisao.java
+++ Exercicios Iniciante/src/iniciante/Divisao.java
@@ -8,7 +8,7 @@ public class Divisao {
 
 	public static void main(String[] args) {
 
-		Scanner dadosUsuario = new Scanner(System.in);
+		/*Scanner dadosUsuario = new Scanner(System.in);
 
 		int primeiroValor = 0;
 		int segundoValor = 1;
@@ -38,6 +38,44 @@ public class Divisao {
 		} else {
 			System.out.println("Valores Informados incorretos.");
 		}
+*/
+		// Com o loop for
+		Scanner dados = new Scanner(System.in);
+
+		int divisor = 0;
+		int dividendo = 1;
+		double resultadoDivisao = 0.0;
+		double restoDivisao = 0.0;
+
+		System.out.println("Siga os passos abaixo.");
+		System.out.println("Digite");
+
+		for (int i = 1; i <= 2; i++) {
+			if (i == 1) {
+				System.out.println(i + "º" + " valor: ");
+				divisor = dados.nextInt();
+			}
+			if (i == 2) {
+				System.out.println(i + "º" + " valor: ");
+				dividendo = dados.nextInt();
+			}
+
+			if (dividendo == 0) {
+				System.out.println("O segundo valor informado é invalido.");
+				dividendo = dados.nextInt();
+			}
+		}
+		if (dividendo != 0) {
+			resultadoDivisao = (divisor / dividendo);
+			restoDivisao = (divisor % dividendo);
+
+			System.out.println(divisor + "/" + dividendo + " = " + resultadoDivisao);
+			System.out.println(divisor + "%" + dividendo + " = " + restoDivisao);
+		} else {
+			System.out.println("Os valores informados são incorretos.");
+		}
+		
+		
 	}
 
 }
