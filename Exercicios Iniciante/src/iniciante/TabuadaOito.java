3ea05ee264dececdb76145e854bec72624bf50a5
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-01-12 00:26:57 -0200

    Foi adicionado o arquivo TabuadaOito.java que mostra o calculo da tabuada.

---
+++ Exercicios Iniciante/src/iniciante/TabuadaOito.java
@@ -0,0 +1,14 @@
+package iniciante;
+
+public class TabuadaOito {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i <= 10; i++) {
+			int resultado = 8 * i;
+			System.out.println(8 + " * " + i + " = " + resultado);
+		}
+
+	}
+
+}
