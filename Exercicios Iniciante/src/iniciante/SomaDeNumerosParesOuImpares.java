d7455ceb3181d1ff0ff9cf2f07b2c0a00b3d4745
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-01-29 01:56:18 -0200

    Foi adicionado o arquivo SomaDeNumerosParesOuImpares.java

---
+++ Exercicios Iniciante/src/iniciante/SomaDeNumerosParesOuImpares.java
@@ -0,0 +1,24 @@
+// Algoritmo que soma dois valores digitados e verifica se o total é par ou Ímpar.
+
+package iniciante;
+
+public class SomaDeNumerosParesOuImpares {
+
+	public static void main(String[] args) {
+
+		int primiroNumero = 23;
+		int segundoNumero = 10;
+		int total = (primiroNumero + segundoNumero);
+
+		if (total != 0) {
+			if (total % 2 == 0) {
+				System.out.println("Total é par: " + total);
+			} else {
+				System.out.println("Total é ímpar: " + total);
+			}
+		} else {
+			System.out.println("Zero é um número neutro, por favor digite outro valor.");
+		}
+	}
+
+}
