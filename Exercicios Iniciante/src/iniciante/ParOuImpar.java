8ca05832ecb5c74d1807a19d98bf63e314ea9031
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-01-29 01:26:09 -0200

    Foi adicionado o arquivo ParOuImpar.java

---
+++ Exercicios Iniciante/src/iniciante/ParOuImpar.java
@@ -0,0 +1,17 @@
+//Algoritmo que verifica se determinado valor é par ou Ímpar.
+
+package iniciante;
+
+public class ParOuImpar {
+	public static void main(String[] args) {
+
+		int numero = 9;
+
+		if (numero % 2 == 0) {
+			System.out.println(numero + " é" + " par.");
+		} else {
+			System.out.println(numero + " é" + " ímpar.");
+		}
+
+	}
+}
