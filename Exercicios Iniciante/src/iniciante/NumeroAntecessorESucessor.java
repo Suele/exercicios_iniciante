33f9587b0acc3d7be8746a8b25be5d5ce114056b
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-01-29 09:59:25 -0200

    Foi adicionado o arquivo NumeroAntecessorESucessor.java

---
+++ Exercicios Iniciante/src/iniciante/NumeroAntecessorESucessor.java
@@ -0,0 +1,18 @@
+//Algoritmo que mostra o antecessor e sucessor de um número.
+
+package iniciante;
+
+public class NumeroAntecessorESucessor {
+
+	public static void main(String[] args) {
+
+		int numero = 1;
+		int antecessor = numero - 1;
+		int sucessor = numero + 1;
+
+		System.out.println("Valor digitado: " + numero);
+		System.out.println("Antecessor: " + antecessor);
+		System.out.println("Sucessor: " + sucessor);
+	}
+
+}
