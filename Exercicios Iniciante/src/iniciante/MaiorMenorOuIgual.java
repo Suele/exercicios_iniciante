f3c86da06e92443ef7e5fb9360071b4a37aae938
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-02-01 23:34:03 -0200

    Foi adicionado o arquivo Divisao.java

--- Exercicios Iniciante/src/iniciante/MaiorMenorOuIgual.java
+++ Exercicios Iniciante/src/iniciante/MaiorMenorOuIgual.java
@@ -9,7 +9,7 @@ public class MaiorMenorOuIgual {
 		int primeiroNumero = 10;
 		int segundoNumero = 10;
 
-		if (primeiroNumero != segundoNumero && primeiroNumero != 0 && segundoNumero != 0) {
+		if ((primeiroNumero != 0 && segundoNumero != 0) && (primeiroNumero != segundoNumero)) {
 			if (primeiroNumero > segundoNumero) {
 				System.out.println(primeiroNumero + " > " + segundoNumero);
 			}
@@ -17,11 +17,11 @@ public class MaiorMenorOuIgual {
 				System.out.println(segundoNumero + " > " + primeiroNumero);
 			}
 
-		} else if (primeiroNumero == segundoNumero) {
+		} else if ((primeiroNumero == segundoNumero) && (primeiroNumero != 0 && segundoNumero != 0)) {
 			System.out.println("Os valores não podem serem iguais.");
-		} else if (primeiroNumero == 0) {
+		} else if (primeiroNumero == 0 && segundoNumero != 0) {
 			System.out.println("O primeiro valor não pode ser zero.");
-		} else if (segundoNumero == 0) {
+		} else if (segundoNumero == 0 && primeiroNumero != 0) {
 			System.out.println("O segundo valor não pode ser zero.");
 		} else if (primeiroNumero == 0 && segundoNumero == 0) {
 			System.out.println(" Os valores não podem serem zero.");
