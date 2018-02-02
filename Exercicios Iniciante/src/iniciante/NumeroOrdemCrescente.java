f543432ccf0022ae6dbf8a672cc66b6e6ca0ad44
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-02-02 10:34:02 -0200

    Foram adicionados os arquivos.

---
+++ Exercicios Iniciante/src/iniciante/NumeroOrdemCrescente.java
@@ -0,0 +1,13 @@
+package iniciante;
+
+public class NumeroOrdemCrescente {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
