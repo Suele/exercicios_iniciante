859a4e1aa43424a618f12459100adb2ccc91c26f
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-02-05 08:02:02 -0200

    Foram deletados os arquivos antigos e adicionados outros.

---
+++ Exercicios Iniciante/src/ParOuImpar.java
@@ -0,0 +1,32 @@
+// Algoritmo que verifica se o número digitado é par ou ímpar.
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+
+	public static void main(String[] args) {
+
+		Scanner lerDados = new Scanner(System.in);
+
+		System.out.println("Digite 3 valores: ");
+		int primeiroNumero = 0;
+
+		for (int i = 1; i <= 3; i++) {
+			
+			System.out.print(">>> ");
+			primeiroNumero = lerDados.nextInt();
+
+			if (primeiroNumero % 2 == 0) {
+				System.out.println(">>> " + primeiroNumero + " é par.");
+			} else {
+				System.out.println(">>> " + primeiroNumero + " é ímpar.");
+			}
+			
+			if (i == 3) {
+				System.out.println(">>> Fim ");
+			}
+		}
+
+	}
+
+}
