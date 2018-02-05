add7ac2a5550b4ad863fb0aaea70e330a3a18f53
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-02-05 09:07:10 -0200

    Foi modificado o arquivo CategoriaDosAtletas.java adicionado novas condições de verificação da idade e o nome.

--- Exercicios Iniciante/src/iniciante/CategoriaDosAtletas.java
+++ Exercicios Iniciante/src/iniciante/CategoriaDosAtletas.java
@@ -6,25 +6,36 @@ import java.util.Scanner;
 public class CategoriaDosAtletas {
 
 	public static void main(String[] args) {
-		
+
 		Scanner lerDados = new Scanner(System.in);
 
 		int idade = 0;
-		
+		String nome;
+
+		System.out.print("Digite seu nome: ");
+		nome = lerDados.nextLine();
+
 		System.out.print("Digite sua idade: ");
 		idade = lerDados.nextInt();
-		
+
+		if (idade < 5) {
+			System.out.println("Idade incompativel com as categorias.");
+		}
+
 		if (idade >= 5 && idade <= 10) {
-			System.out.println("Infantil");
+			System.out.println("Nome: " + nome + " Categoria Infantil");
 		}
 		if (idade >= 11 && idade <= 15) {
-			System.out.println("Juvenil");
+			System.out.println("Nome: " + nome + " Categoria Juvenil");
 		}
 		if (idade >= 16 && idade <= 20) {
-			System.out.println("Junior");
+			System.out.println("Nome: " + nome + " Categoria Junior");
 		}
-		if(idade >= 21 && idade >= 25) {
-			System.out.println("Profissional");
+		if ((idade >= 21 && idade >= 25 && idade <= 42)) {
+			System.out.println("Nome: " + nome + " Categoria Profissional");
+		}
+		if (idade > 42) {
+			System.out.println("Aposentado");
 		}
 	}
 
